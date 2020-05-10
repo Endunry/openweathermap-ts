786b9ef02650f69be6537aa949a978424f054933
shimphillip <shimphillip@gmail.com>
2020-05-10 18:09:08 -0500

    basic setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,41 @@
-console.log('hi');
+console.log('hi')
+
+// pi.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+
+// api.openweathermap.org/data/2.5/weather?q={city name},{state}&appid={your api key}
+
+// api.openweathermap.org/data/2.5/weather?q={city name},{state},{country code}&appid={your api key}
+
+interface IConfig {
+  apiKey: string
+  units?: string
+  langauge?: string
+}
+
+class OpenWeatherMap {
+  public apiKey: string
+  public units: string
+  public langauge: string
+
+  constructor({ apiKey, units = 'imperial', langauge = 'en' }: IConfig) {
+    this.apiKey = apiKey
+    this.units = units
+    this.langauge = langauge
+  }
+
+  public getApiKey() {
+    return this.apiKey
+  }
+
+  public getUnits() {
+    return this.units
+  }
+
+  public getLanguage() {
+    return this.langauge
+  }
+}
+
+const newMap = new OpenWeatherMap({
+  apiKey: 'asdfasdfasdf',
+})
