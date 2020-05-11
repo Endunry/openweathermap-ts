8309a66f94117963fd3c789590552d828320a626
shimphillip <shimphillip@gmail.com>
2020-05-10 23:53:26 -0500

    get current weather

--- src/app.ts
+++ src/app.ts
@@ -21,8 +21,12 @@ interface IinitialSettings extends ISettings {
   apiKey: string
 }
 
+const host = `https://api.openweathermap.org/data/`
+const apiVersion = `2.5/`
+
 class OpenWeatherMap {
   private settings: IinitialSettings
+  private baseURL: string
 
   constructor({
     apiKey,
@@ -34,6 +38,7 @@ class OpenWeatherMap {
       units,
       language,
     }
+    this.baseURL = host + apiVersion
   }
 
   public getSettings(key?: string) {
@@ -60,6 +65,76 @@ class OpenWeatherMap {
 
     return `Please pass in an option key with a value`
   }
+
+  // Getters
+
+  public async getCurrentWeatherByCityName(cityName: string) {
+    try {
+      const { baseURL, settings } = this
+
+      const endPoint = `${baseURL}weather?q=${cityName}&appid=${settings.apiKey}`
+      const response = await fetch(endPoint)
+      const currentWeather = await response.json()
+
+      console.log(currentWeather)
+      return currentWeather
+    } catch (error) {
+      return error
+    }
+  }
+
+  public async getCurrentWeatherByCityId(cityId: number) {
+    try {
+      const { baseURL, settings } = this
+
+      const endPoint = `${baseURL}weather?id=${cityId}&appid=${settings.apiKey}`
+      const response = await fetch(endPoint)
+      const currentWeather = await response.json()
+
+      console.log(currentWeather)
+      return currentWeather
+    } catch (error) {
+      return error
+    }
+  }
+
+  public async getCurrentWeatherByGeoCoordinates(
+    latitude: number,
+    longitude: number
+  ) {
+    try {
+      const { baseURL, settings } = this
+
+      const endPoint = `${baseURL}weather?lat=${latitude}&lon=${longitude}&appid=${settings.apiKey}`
+      const response = await fetch(endPoint)
+      const currentWeather = await response.json()
+
+      console.log(currentWeather)
+      return currentWeather
+    } catch (error) {
+      return error
+    }
+  }
+
+  public async getCurrentWeatherByZipcode(
+    zipcode: number,
+    countryCode?: number
+  ) {
+    try {
+      const { baseURL, settings } = this
+
+      const endPoint = `${baseURL}weather?zip=${zipcode}${
+        countryCode ? ',' + countryCode : ''
+      }&appid=${settings.apiKey}`
+      const response = await fetch(endPoint)
+      const currentWeather = await response.json()
+
+      console.log(currentWeather)
+      return currentWeather
+    } catch (error) {
+      return error
+    }
+  }
 }
 
 // tests
@@ -69,6 +144,10 @@ const newMap = new OpenWeatherMap({
 
 console.log(newMap.getSettings())
 console.log(newMap.setSettings('language', 'kr'))
+// newMap.getCurrentWeatherByCityName('austin')
+// newMap.getCurrentWeatherByCityId(300)
+// newMap.getCurrentWeatherByGeoCoordinates(30.2672, 97.7431)
+// newMap.getCurrentWeatherByZipcode(78754)
 // console.log(newMap.setApiKey('qwpeorqjwe'))
 
 export default OpenWeatherMap
